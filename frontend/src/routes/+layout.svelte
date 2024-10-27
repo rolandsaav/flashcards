1fbc661d3d2e6fc02e84fb2cc238fee4cfb6e095
Roland Saavedra <rolandsaavedra22@gmail.com>
2024-10-27 01:38:57 -0400

    Created nav layout

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<h1 class="navbar">
+    <a href="/">Home</a>
+    <a href="/login">Login</a>
+</h1>
+
+<slot></slot>
+
+<style>
+    .navbar {
+        display: flex;
+        gap: 1rem;
+    }
+</style>
